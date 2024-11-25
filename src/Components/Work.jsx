6a2f3c4b7c5fdfb1f22d57576851bdd30c9c62d8
Riajul Islam <riajulpislam@gmail.com>
2024-11-25 15:41:53 +0600

    work-image-update

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -3,7 +3,7 @@
 function Work() {
   var images =[
     {url:"https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/66bf07ff7266875aeb792e36_All%20Voices%20-%204%203.webp",top:"50%", let:"50%", isActive:false},
-    {url:"https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d8b3d1998e49106060db_Maniv%20-%204%203.webp", top:"56%", let:"44%", isActive:true},
+    {url:"https://cdn.prod.website-files.com/62df9251ae9124d9d726bcb9/6346b68e7f858311216e120b_V2%20Tools%20OpenGraph%20Final.jpg", top:"56%", let:"44%", isActive:true},
     {url:"https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d81b5919f738bad40a7c_Silvr%20-4%203.webp", top:"45%", let:"56%", isActive:false},
     {url:"https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d405786d15acd0e22a91_Kablamo%20-%204%203.webp", top:"60%", let:"53%", isActive:false},
   ]
